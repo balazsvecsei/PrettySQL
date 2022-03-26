f18f4eca411ffafb5a4a92db69c5ba0e7e010290
Jónás Eszter <balazs.vecsei@gmail.com>
2022-03-26 17:05:57 +0100

    setColumn

--- src/QueryBuilder/Processes/CreateProcess.php
+++ src/QueryBuilder/Processes/CreateProcess.php
@@ -8,13 +8,9 @@ class CreateProcess extends AbstractProcess
 {
     public $query;
     public $tableName;
-    public const INIT_METHOD = "CREATE TABLE";
+    public const CREATE = "CREATE TABLE";
 
     private $columns = [];
-
-    public function __construct()
-    {
-    }
 
     public function setTable($name)
     {
@@ -29,16 +25,23 @@ class CreateProcess extends AbstractProcess
 
     public function createQuery()
     {
-        $query = self::INIT_METHOD . " " . $this->tableName;
-
-        $columns = "";
+        $query = self::CREATE . " " . $this->tableName;
 
         if (!empty($this->columns)) {
-            $columns = array_map(fn ($column) => implode(" ", [
-                "name" => $column["name"],
-                "key" => $column["key"],
-                "type" => $column["type"],
-            ]), $this->columns);
+            $columns = array_map(
+                function ($column) {
+                    return implode(" ", [
+                        "name" => $column["name"],
+                        "key" => $column["key"] ? "" : "",
+                        "type" => $column["type"],
+                        "size" => $column["size"],
+                        "nullable" => $column["nullable"] ? "" : "NOT NULL",
+                        "uniqe" => $column["uniqe"] ? "UNIQUE" : "",
+
+                    ]);
+                },
+                $this->columns
+            );
             $columns = implode(", ", $columns);
             $query = "$query ($columns)";
         }
@@ -48,29 +51,47 @@ class CreateProcess extends AbstractProcess
         return $this;
     }
 
-
-
-    public function addColumn($name, $key, $type, $size, $nullable)
+    public function addColumn($name, $key, $type, $size, $nullable, $uniqe)
     {
         array_push($this->columns, [
-            "name" => $name,
-            "key" => $key,
-            "type" => $type,
-            "size" => $size,
-            "nullable" => $nullable,
+            "col_name" => $name,
+            "col_key" => $key,
+            "col_type" => $type,
+            "col_size" => $size,
+            "col_nullable" => $nullable,
+            "col_uniqe" => $uniqe,
         ]);
+        return $this;
+    }
+
+
+
+    public function setColumn($columnName, $attributes)
+    {
+        $selectedColumnIndex = array_search("'$columnName'", array_column($this->columns, "col_name"));
+
+        $column = $this->columns[$selectedColumnIndex];
+
+        foreach ($attributes as $key => $value)
+            if (array_key_exists($key, $attributes))
+                $column[$key] = $value;
+            else
+                return false;
+
+        $this->columns[$selectedColumnIndex] = $column;
+
         return $this;
     }
 
     public function integer($columnName)
     {
-        $this->addColumn("'$columnName'", null, "INT", null, true);
+        $this->addColumn("'$columnName'", false, "INT", null, true, false);
         return $this;
     }
 
     public function string($columnName, $size = 255)
     {
-        $this->addColumn("'$columnName'", null, "VARCHAR($size)", null, true);
+        $this->addColumn("'$columnName'", null, "VARCHAR($size)", null, true, false);
         return $this;
     }
 
--- tests/PrettyTest.php
+++ tests/PrettyTest.php
@@ -1,22 +1,27 @@
-<?php
+earl<?php
 
-use PHPUnit\Framework\TestCase;
-use Prettysql\PSql;
+    use PHPUnit\Framework\TestCase;
+    use Prettysql\PSql;
 
-class PrettyTest extends TestCase
-{
-    public $prettySql;
-    public function test_prettySql_object()
+    class PrettyTest extends TestCase
     {
-        $prettySql = new PSql();
+        public $prettySql;
 
-        $this->assertIsObject($prettySql);
+        public function __construct()
+        {
+            $this->prettySql = new PSql();
+        }
+
+        public function test_prettySql_object()
+        {
+
+            $this->assertIsObject($this->prettySql);
+        }
+
+        public function test_create_table()
+        {
+            $table = (new PSql())->create("users")->getQuery();
+
+            $this->assertIsString($table);
+        }
     }
-
-    public function test_create_table()
-    {
-        $table = (new PSql())->create("users")->getQuery();
-
-        $this->assertIsString($table);
-    }
-}
